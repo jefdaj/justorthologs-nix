fec4feadab5a97f15f1c56d8cc7832dcdae8d34e
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-02 11:58:19 -0700

    got all but one test passing

--- default.nix
+++ default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
     biopython
   ];
 
-  phases = "installPhase fixupPhase";
+  phases = "installPhase fixupPhase installCheckPhase";
 
   #unpackPhase = ''
     #source $stdenv/setup
@@ -41,5 +41,21 @@ buildPythonPackage rec {
       patchShebangs "$script"
     done
     buildPythonPath "$out"
+  '';
+
+  installCheckPhase = ''
+    source $stdenv/setup
+    set -x
+    cd $TMPDIR
+
+    python $out/bin/justOrthologs.py -q ${src}/smallTest/orthologTest/bonobo.fa -s ${src}/smallTest/orthologTest/human.fa -o output -c && head -n3 output; rm -rf output*
+    python $out/bin/combineOrthoGroups.py -id ${src}/smallTest/testCombineOrthoGroups/ -o output && head -n3 output; rm -rf output*
+    python $out/bin/gff3_parser.py -g ${src}/smallTest/wrapperTest/small_human.gff3 -f ${src}/smallTest/wrapperTest/small_human.fasta.gz -o output.fasta && head -n3 output.fasta; rm -rf output*
+    python $out/bin/getNoException.py -i ${src}/smallTest/testNoException/test.fasta -o output.fasta && head -n3 output.fasta; rm -rf output*
+
+    # TODO fix this one
+    # python $out/bin/wrapper.py -g1 ${src}/smallTest/wrapperTest/small_pan.gff3 -g2 ${src}/smallTest/wrapperTest/small_human.gff3 -r1 ${src}/smallTest/wrapperTest/small_pan.fasta.gz -r2 ${src}/smallTest/wrapperTest/small_human.fasta.gz -all -o output && head -n3 output; rm -rf output*
+
+    set +x
   '';
 }
