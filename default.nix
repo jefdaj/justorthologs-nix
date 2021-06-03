5a7f505cb1df91f240da887d1dc5f0dc003a6f85
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-03 13:23:54 -0700

    buildPythonPackage -> mkDerivation

--- default.nix
+++ default.nix
@@ -1,19 +1,18 @@
 { stdenv
 , fetchurl
 , makeWrapper
-, buildPythonPackage
-, biopython
+, python
 }:
 
-buildPythonPackage rec {
+stdenv.mkDerivation rec {
   name = "justorthologs-${version}";
   version = "7ab9104";
   src = ./.;
-  propagatedBuildInputs = [
-    biopython
-  ];
 
-  phases = "installPhase fixupPhase installCheckPhase";
+  # this python needs to have biopython 1.76 available
+  buildInputs = [ makeWrapper python ];
+
+  phases = "installPhase fixupPhase checkPhase";
 
   #unpackPhase = ''
     #source $stdenv/setup
@@ -33,28 +32,30 @@ buildPythonPackage rec {
       chmod +x "$script"
       patchShebangs "$script"
       substituteInPlace $script --replace "#!/nix" "#!/usr/bin/env /nix"
+      wrapProgram "$script" --prefix PATH : "${python}/bin"
     done
-    buildPythonPath "$out"
   '' else ''
     for script in $out/bin/*.py; do
       chmod +x "$script"
       patchShebangs "$script"
+      wrapProgram "$script" --prefix PATH : "${python}/bin"
     done
-    buildPythonPath "$out"
   '';
 
-  installCheckPhase = ''
+  doCheck = true;
+  checkPhase = ''
     source $stdenv/setup
+    set -e
     set -x
     cd $TMPDIR
 
-    python $out/bin/justOrthologs.py -q ${src}/smallTest/orthologTest/bonobo.fa -s ${src}/smallTest/orthologTest/human.fa -o output -c && head -n3 output; rm -rf output*
-    python $out/bin/combineOrthoGroups.py -id ${src}/smallTest/testCombineOrthoGroups/ -o output && head -n3 output; rm -rf output*
-    python $out/bin/gff3_parser.py -g ${src}/smallTest/wrapperTest/small_human.gff3 -f ${src}/smallTest/wrapperTest/small_human.fasta.gz -o output.fasta && head -n3 output.fasta; rm -rf output*
-    python $out/bin/getNoException.py -i ${src}/smallTest/testNoException/test.fasta -o output.fasta && head -n3 output.fasta; rm -rf output*
+    $out/bin/justOrthologs.py -q ${src}/smallTest/orthologTest/bonobo.fa -s ${src}/smallTest/orthologTest/human.fa -o output -c && head -n3 output; rm -rf output*
+    $out/bin/combineOrthoGroups.py -id ${src}/smallTest/testCombineOrthoGroups/ -o output && head -n3 output; rm -rf output*
+    $out/bin/gff3_parser.py -g ${src}/smallTest/wrapperTest/small_human.gff3 -f ${src}/smallTest/wrapperTest/small_human.fasta.gz -o output.fasta && head -n3 output.fasta; rm -rf output*
+    $out/bin/getNoException.py -i ${src}/smallTest/testNoException/test.fasta -o output.fasta && head -n3 output.fasta; rm -rf output*
 
-    # TODO fix this one
-    # python $out/bin/wrapper.py -g1 ${src}/smallTest/wrapperTest/small_pan.gff3 -g2 ${src}/smallTest/wrapperTest/small_human.gff3 -r1 ${src}/smallTest/wrapperTest/small_pan.fasta.gz -r2 ${src}/smallTest/wrapperTest/small_human.fasta.gz -all -o output && head -n3 output; rm -rf output*
+    # TODO figure out this one
+    # $out/bin/wrapper.py -g1 ${src}/smallTest/wrapperTest/small_pan.gff3 -g2 ${src}/smallTest/wrapperTest/small_human.gff3 -r1 ${src}/smallTest/wrapperTest/small_pan.fasta.gz -r2 ${src}/smallTest/wrapperTest/small_human.fasta.gz -all -o output && head -n3 output; rm -rf output*
 
     set +x
   '';
--- release.nix
+++ release.nix
@@ -3,9 +3,9 @@ let
   pkgs = import sources.nixpkgs {};
 
   # TODO any of the other pydeps from treecl-nix required here for biopython to function?
-  myPython27Packages = pkgs.python27Packages // rec {
-    biopython          = pkgs.python27Packages.callPackage ./nix/pydeps/biopython {};
+  myPython27Packages = pkgs.python27Packages // {
+    biopython = pkgs.python27Packages.callPackage ./nix/pydeps/biopython {};
   };
+  myPython = myPython27Packages.python.withPackages (_: [ myPython27Packages.biopython ]);
 
-in
-  myPython27Packages.callPackage ./default.nix { inherit (myPython27Packages) biopython; }
+in pkgs.callPackage ./default.nix { python = myPython; }
